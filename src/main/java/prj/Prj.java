fb27f89bc9977cfeefa16a6a3736618a76d1a5ff
Acryval <wojholf.98@gmail.com>
2022-09-10 19:08:51 +0200

    default own server

--- src/main/java/prj/Prj.java
+++ src/main/java/prj/Prj.java
@@ -53,7 +53,7 @@ public class Prj extends JFrame{
             }
         });
 
-        gsm.setState("client", new SetUsernamePacket("Acw"), new ConnectToServerPacket(new InetSocketAddress("25.68.34.237", 53912)), new PassDataPacket(), new ScreenDimensionPacket(width, height));
+        gsm.setState("client", new SetUsernamePacket("Acryval"), new StartServerPacket(), new PassDataPacket(), new ScreenDimensionPacket(width, height));
         gsm.run();
     }
 
