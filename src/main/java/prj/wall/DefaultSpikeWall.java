ef9481e3c0aca0f5e55c58d83670ace856834650
MBanaszkiewiczZ <mbanaszkiewicz2000@gmail.com>
2022-09-11 14:29:36 +0200

    obrażenia od kolców - test

--- src/main/java/prj/wall/DefaultSpikeWall.java
+++ src/main/java/prj/wall/DefaultSpikeWall.java
@@ -6,9 +6,9 @@ import java.awt.*;
 public class DefaultSpikeWall extends Wall{
 
     public DefaultSpikeWall(int x, int y) {
-        super(x, y, 50, 50, false, true, false, "DefaultSpikeWall");
+        super(x, y, 50, 50, false, true, true, "DefaultSpikeWall");
         super.setHitbox(new Rectangle(x, y, 50, 50));
-        this.setDamage(100);
+        this.setDamage(50);
         loadImage();
     }
     @Override
