e303cbfb8ce51c7bac09ee4d2e598aaf8f3e8cb6
MBanaszkiewiczZ <mbanaszkiewicz2000@gmail.com>
2022-09-11 10:38:50 +0200

    kolce- test

--- src/main/java/prj/wall/DefaultSpikeWall.java
+++ src/main/java/prj/wall/DefaultSpikeWall.java
@@ -1,24 +1,23 @@
-package com.company;
+package prj.wall;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class DefaultSpikeWall extends Wall{
 
-    public DefaultSpikeWall(int x, int y, GamePanel panel) {
-        super(x, y, 50, 50, false, true, false, panel);
+    public DefaultSpikeWall(int x, int y) {
+        super(x, y, 50, 50, false, true, false, "DefaultSpikeWall");
         super.setHitbox(new Rectangle(x, y, 50, 50));
         this.setDamage(100);
         loadImage();
     }
     @Override
     public void loadImage() {
-        ImageIcon ii = new ImageIcon("SpikePrototype.png");
-        super.getImage().add(ii.getImage());
+        super.getImage().add("SpikePrototype");
     }
 
     @Override
     public void draw(Graphics2D g2d) {
-        g2d.drawImage(this.getImage().get(0), super.getX(), super.getY(), super.getPanel());
+        g2d.drawImage(super.getImage(0), super.getX(), super.getY(), null);
     }
 }
